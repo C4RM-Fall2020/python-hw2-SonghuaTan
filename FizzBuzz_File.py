ca17cdabdeb074f1c4cd4c709147eb90ac10510d
SonghuaTan <st3834@columbia.edu>
2026-07-06 10:53:19 +0800

    Update FizzBuzz_File.py

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -1,5 +1,14 @@
 import numpy as np
 
 def FizzBuzz(start, finish):
-    v = ['buzz', 41, 'fizz', 43, 44, 'fizzbuzz']
-    return(v)
+
+    v = np.arange(start, finish + 1, dtype=object)
+
+    fizz = (v % 3 == 0)
+    buzz = (v % 5 == 0)
+
+    v[fizz & ~buzz] = 'fizz'
+    v[buzz & ~fizz] = 'buzz'
+    v[fizz & buzz] = 'fizzbuzz'
+
+    return list(v)
