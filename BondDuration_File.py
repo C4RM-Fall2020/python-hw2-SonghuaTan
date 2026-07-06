ca6217ac1535c8ae3df6ed83052f6c64911a6bb8
SonghuaTan <st3834@columbia.edu>
2026-07-06 10:52:06 +0800

    Update BondDuration_File.py

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -1,8 +1,18 @@
 import numpy as np
 
-def getBondDuration(y, face, couponRate, m, ppy = 1):
-    if ppy == 1:
-        x = 8.51
-    if ppy == 2:
-        x = 8.42
-    return(x)
+def getBondDuration(y, face, couponRate, m, ppy=1):
+
+    t = np.arange(1, m * ppy + 1)
+
+    coupon = face * couponRate / ppy
+
+    cashflow = np.full(m * ppy, coupon)
+    cashflow[-1] += face
+
+    pv = 1 / (1 + y / ppy) ** t
+
+    pvcf = cashflow * pv
+
+    duration = np.sum((t / ppy) * pvcf) / np.sum(pvcf)
+
+    return duration
